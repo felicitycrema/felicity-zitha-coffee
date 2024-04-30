0004d9e2208cd6b7dce52e905e11921a6ec4720d
Felicity Zithae <felicitycremau@gmail.com>
2024-04-30 15:15:18 +0200

    first commit

---
+++ coffee.js
@@ -0,0 +1,6 @@
+// function myfunctionBC() {
+//     document.getElementById("blackCoffee").innerHTML = "Black Cofee.";
+// }
+// function myfunctionWC() {
+//     document.getElementById("whiteCoffee").innerHTML = "White Cofee.";
+// }
